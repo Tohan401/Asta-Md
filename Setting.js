4ce543a4b98969b5904c090089a030db427a98b8
Tohan401 <victorosatohanmwe@gmail.com>
2025-05-04 14:33:00 +0100

    Update Setting.js

--- Setting.js
+++ Setting.js
@@ -21,7 +21,7 @@ global.waUrl = process.env.WAURL ||"",
 global.website = '' ; 
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || '' ;
 module.exports = {
-  sessionName: process.env.SESSION_ID || '',
+  sessionName: process.env.SESSION_ID || '',𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=njQVlahC#iNx_341jLmmrcJVd6tJ0npsD3Hi602VgapJTNV13tkE
   botname: process.env.BOT_NAME || 'Asta-Md',
   ownername:  process.env.OWNER_NAME || `Astro`,
   author:  process.env.PACK_AUTHER || 'Astro', 
